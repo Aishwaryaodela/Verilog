cf129d99e44755e0d1a4935f94e9fcd68f002b69
Aishwaryaodela <133080176+Aishwaryaodela@users.noreply.github.com>
2023-05-13 19:24:11 +0530

    Create clock_gen.v

---
+++ clock_gen.v
@@ -0,0 +1,10 @@
+/*------generating clock without using an always block-----*/
+module clock_gen();
+  reg clk;
+  initial 
+    begin
+       clk = 1'b0;
+       forever #30 clk = ~clk;
+    end
+  initial #1000 $finish;
+endmodule
