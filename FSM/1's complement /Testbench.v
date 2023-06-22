671a375aa74399f62c711759e7190468411e049b
Aishwaryaodela <133080176+Aishwaryaodela@users.noreply.github.com>
2023-06-22 07:54:28 +0530

    Update Testbench.v

--- FSM/1's complement /Testbench.v
+++ FSM/1's complement /Testbench.v
@@ -1,5 +1,15 @@
 module one_comptb;
   reg in_data;
-  always clk = ~clk;
+  always #5 clk = ~clk;
+  one_comp DUT(clk,rst,in_data,out_data);
   initial begin
-    clk = 1'b0; rst = 1'b1
+    clk = 1'b0; rst = 1'b0; 
+    #5 rst = 1'b1; in_data =1'b1;
+    #10 in_data = 1'b0;
+    #10 in_data = 1'b1;
+  end
+  initial begin
+    $monitor("clk = %0d, in_data = %0d,out_data = %0d" ,clk,in_data,out_data); 
+    #60 $finish;
+  end
+endmodule
