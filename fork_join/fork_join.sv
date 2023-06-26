aa1fac8ea1823fb0ebd39b223ba07f73797c0712
Aishwaryaodela <133080176+Aishwaryaodela@users.noreply.github.com>
2023-06-26 23:25:41 +0530

    Create fork_join.sv

---
+++ fork_join/fork_join.sv
@@ -0,0 +1,19 @@
+module fork_join();
+  initial begin
+    fork 
+      begin
+        #5 $display("%0d display A start of the display",$time);
+        #10 $display ("%0d display A end of the block",$time); 
+                    end
+                    begin
+                      #8 $display("%0d display B start of the display",$time);
+                      #12 $display ("%0d display b end of the block",$time); 
+                    end
+                    begin
+                      #15 $display("%0d display C start of the display",$time);
+                      #20 $display ("%0d display C end of the block",$time); 
+                    end
+    join
+    $display("After fork_join block",$time); 
+   end
+endmodule 
