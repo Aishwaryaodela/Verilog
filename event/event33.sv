f4bba48c8d4d5c7fcad520c8ec4456f8e77a867d
Aishwaryaodela <133080176+Aishwaryaodela@users.noreply.github.com>
2023-06-27 20:38:23 +0530

    Create event33.sv

---
+++ event/event33.sv
@@ -0,0 +1,17 @@
+module events; 
+  event e1;
+  initial begin
+    fork
+      begin
+        $display("@%0d before triggering even A",$time);
+        ->e1;
+        $display("@%0d after triggering event A",$time);
+      end
+      begin
+        $display("@%0d before triggering even B",$time);
+        wait(e1.triggered);
+        $display("@%0d after triggering event B",$time);
+      end
+    join
+  end
+endmodule
